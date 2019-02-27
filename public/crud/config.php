5e0ca13f1d126b9e5c98bc8599ccff2c7c903b5b
vincent Robin <v.robin59@gmail.com>
2019-02-27 12:46:44 +0100

    -

--- public/crud/config.php
+++ public/crud/config.php
@@ -5,7 +5,7 @@
 
 // Connexion à la BDD
 try {
-    $db = new PDO("mysql:host=127.0.0.1;port=3306;dbname=site_a;charset=utf8", "root", "myosw3sql");
+    $db = new PDO("mysql:host=127.0.0.1;port=3306;dbname=site_a;charset=utf8", "root", "");
 } catch (PDOExecption $e) {
     echo $e->getMessage();
     die;
